922856175940e22014ba25d21e06171e6a5c7629
Marcos Vidal <markos_ari@hotmail.com>
2024-03-21 21:08:50 -0300

    FIX space

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ function Card({
         <div className="card__container">
             <img src={img} alt={shortDesc} />
             <div className='card__data'>
-                <h2>{name} <span>( {brand} )</span></h2>
+                <h2>{name} <span>({brand})</span></h2>
                 <b>{category}</b>
                 <p>{shortDesc}</p>
                 {delivery ? <p>Envío sin cargo.</p> : undefined}
